5f9adcb374b0a99f00a9cae7ebe1bf7467a27544
Liam Collod <lcollod@gmail.com>
2024-06-15 14:20:17 +0200

    refacto(main): make logging configuration optional
    better for unittests

--- knots_hub/__main__.py
+++ knots_hub/__main__.py
@@ -11,19 +11,7 @@ from knots_hub.constants import IS_APP_FROZEN
 LOGGER = logging.getLogger(__name__)
 
 
-def main(argv: Optional[List[str]] = None):
-    """
-    Args:
-        argv: command line arguments. from sys.argv if not provided
-    """
-
-    config = knots_hub.HubConfig.from_environment()
-    filesystem = knots_hub.HubInstallFilesystem(root=config.local_install_path)
-    filesystem.root.mkdir(exist_ok=True)
-
-    cli = knots_hub.get_cli(argv=argv, config=config, filesystem=filesystem)
-
-    log_level = logging.DEBUG if cli.debug else logging.INFO
+def configure_logging(log_level, log_path):
 
     formatter = logging.Formatter(
         "{levelname: <7} | {asctime} [{name}] {message}",
@@ -38,7 +26,7 @@ def main(argv: Optional[List[str]] = None):
     logging.root.addHandler(handler)
 
     handler = logging.handlers.RotatingFileHandler(
-        filesystem.log_path,
+        log_path,
         maxBytes=65536,
         # need at least one backup to rotate
         backupCount=1,
@@ -48,10 +36,29 @@ def main(argv: Optional[List[str]] = None):
     handler.setFormatter(formatter)
     logging.root.addHandler(handler)
 
+
+def main(argv: Optional[List[str]] = None, logging_configuration: bool = False):
+    """
+    Args:
+        argv: command line arguments. from sys.argv if not provided
+        logging_configuration: True to configure logging system.
+    """
+
+    config = knots_hub.HubConfig.from_environment()
+    filesystem = knots_hub.HubInstallFilesystem(root=config.local_install_path)
+    filesystem.root.mkdir(exist_ok=True)
+
+    cli = knots_hub.get_cli(argv=argv, config=config, filesystem=filesystem)
+
+    log_level = logging.DEBUG if cli.debug else logging.INFO
+    if logging_configuration:
+        configure_logging(log_level=log_level, log_path=filesystem.log_path)
+
     LOGGER.debug(
         f"starting {knots_hub.__name__} v{knots_hub.__version__} (frozen={IS_APP_FROZEN})"
     )
     LOGGER.debug(f"retrieved cli with args={cli._args}")
+    LOGGER.debug(f"config={config}")
 
     if not OS.is_windows():
         OS.raise_unsupported()
@@ -60,4 +67,4 @@ def main(argv: Optional[List[str]] = None):
 
 
 if __name__ == "__main__":
-    main()  # pragma: no cover
+    main(logging_configuration=True)  # pragma: no cover
